cfff9f6e1ac6a1dc02b93555ad79e29059c6dea4
stharshine <tharushaheenatigala@gmail.com>
2022-12-28 17:14:24 +0000

    coursework part 2

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "6506f0ab29e55494ab5ba2f79ea1c628"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "c8cc898e896545aab2de74b3f562552f"
